0bc6e4bf665207f1044a6e84143d44fb3834a767
Andry Tumaruk <zeronime17@gmail.com>
2026-09-07 02:18:22 +0700

    refactor:membuat performance data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,25 +2,25 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use App\Models\Category;
 
 class CategoryController extends Controller
 {
+    //
     public function index()
     {
         return view('categories', [
-            'title' => 'Post Categories',
-            'categories' => Category::all()
+            'title' => 'post categories',
+            'categories' => Category::all(),
         ]);
     }
-
     public function show(Category $category)
     {
-        return view('category', [
-            'title' => 'post by category '.$category->name,
-            'posts' => $category->post,
-            'category' => $category->name,
+        return view('posts', [
+            'title' => "post by categories: $category->name",
+            'posts' => $category->post->load('author', 'category'),
         ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 ﻿@extends('layouts.main')
 
 @section('container')
-<h1>halaman blog posts</h1>
+<h1>{{$title}}</h1>
 
 @foreach ($posts as $post)
 
