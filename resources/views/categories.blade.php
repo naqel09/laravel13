93141125efc4d38bf4497545236be9aea4da28f3
Andry Tumaruk <zeronime17@gmail.com>
2026-09-09 16:01:19 +0700

    feat: membuat pagination dan searching

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         @foreach ($categories as $category)
         <div class="col-md-4">
-            <a href="/categories/{{ $category->slug }}">
+            <a href="/posts?category={{ $category->slug }}">
             <div class="card text-bg-dark">
                 <img src="https://images.unsplash.com/photo-1638602612226-55fd638475c9?q=80&w=875&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img" alt="...">
                 <div class="card-img-overlay d-flex align-items-center p-0">
